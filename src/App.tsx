34703169ba74eff1ca80bd022006abb943266cbd
Nguyen Dinh Hoang Anh <ndha1115@gmail.com>
2024-08-21 13:03:08 +0700

    initial project

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import ProductCard from "./components/products/ProductCard"
+
+
+
+function App() {
+
+  return (
+    <>
+     Hello world
+     <ProductCard/>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/products/ProductCard.tsx
@@ -0,0 +1,9 @@
+import './product-card.scss';
+
+const ProductCard = () => {
+    return <div className="card-container">
+
+    </div>
+}
+
+export default ProductCard;
